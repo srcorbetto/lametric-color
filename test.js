f328eea3cec56bef3607b244e85af1a44ba7afa8
Mike Corbett <srcorbetto@gmail.com>
2022-01-31 18:31:17 -0600

    testing words

--- test.js
+++ test.js
@@ -57,3 +57,56 @@ getColor(randomColor());
 })();
 
 // Icon ID: 48463
+
+// app.get("/color", (req, res) => {
+//     const color = randomColor();
+//     const trimmedColor = color.substring(1);
+//     (async () => {
+//       await openBrowser();
+//       await goto("https://developer.lametric.com/login");
+//       await click($("#email"));
+//       await write(process.env.EMAIL);
+//       await click($("#password"));
+//       await write(process.env.PASS);
+//       await press("Enter");
+//       await goto("https://developer.lametric.com/applications/info/28948");
+//       await click($(".application-icon"));
+//       await click($("#animationTextFilter"));
+//       await write("LaMetric Color");
+//       await click($(".one"));
+//       await click($(".edit"));
+//       await click($(".current-color-input"), {
+//         clickCount: 3,
+//       });
+//       await write(color);
+//       await press("Enter");
+//       await click($(".set-all"));
+//       await click($(".save-image"));
+//       await evaluate(link("select"), (ele) => ele.click());
+//       await reload("https://developer.lametric.com/applications/info/28948");
+//       await click(link("update"));
+//       await closeBrowser().then(console.log("Complete"));
+//     })().then(
+//       axios
+//         .get(`https://www.thecolorapi.com/id?hex=${trimmedColor}`)
+//         .then((colorApiRes) => {
+//           const colorName = colorApiRes.data.name.value;
+//           const colorHex = colorApiRes.data.hex.value;
+//           const data = {
+//             frames: [
+//               {
+//                 text: colorName,
+//                 icon: 48463,
+//                 index: 0,
+//               },
+//               {
+//                 text: colorHex,
+//                 icon: 48463,
+//                 index: 1,
+//               },
+//             ],
+//           };
+//           res.send(data);
+//         })
+//     );
+//   });
