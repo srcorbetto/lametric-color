0874b602bb2dc262c3139ed9ccd6fdd54cdcaee2
Mike Corbett <srcorbetto@gmail.com>
2022-02-05 10:37:15 -0600

    Cleaning up README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# lametric-color
-Displays a random color on LaMetric smart clock
+# Lametric Color
+LaMetric app that displays a random color and its hex value every minute.
+<img src="https://via.placeholder.com/600x300" alt="drawing"/>
+&nbsp;
+### App installation
+Search Random Color on the LaMetric app store.
+<img src="app-install-instructions.gif" alt="drawing" height="750"/>
+&nbsp;
+### To run locally
+* Clone the repo
+* Install dependencies
+* Run `npm start`
+
--- server.js
+++ server.js
@@ -30,11 +30,42 @@ app.get("/color", (req, res) => {
           colorBinary = Buffer.from(response.data, "binary").toString("base64");
           data = {
             frames: [
-              //   {
-              //     text: colorName,
-              //     icon: `data:image/png;base64,${colorBinary}`,
-              //     index: 0,
-              //   },
+              {
+                text: trimmedColor,
+                icon: `data:image/png;base64,${colorBinary}`,
+                index: 1,
+              },
+            ],
+          };
+          res.send(data);
+        });
+    });
+});
+
+app.get("/name", (req, res) => {
+  const color = randomColor();
+  const trimmedColor = color.substring(1);
+  let colorName;
+  let colorHex;
+  let colorUrl;
+  let colorBinary;
+  let data;
+  axios
+    .get(`https://www.thecolorapi.com/id?hex=${trimmedColor}`)
+    .then((colorApiRes) => {
+      colorName = colorApiRes.data.name.value;
+      colorHex = colorApiRes.data.hex.value;
+      colorUrl = `https://singlecolorimage.com/get/${trimmedColor}/8x8`;
+    })
+    .then(() => {
+      axios
+        .get(colorUrl, {
+          responseType: "arraybuffer",
+        })
+        .then((response) => {
+          colorBinary = Buffer.from(response.data, "binary").toString("base64");
+          data = {
+            frames: [
               {
                 text: trimmedColor,
                 icon: `data:image/png;base64,${colorBinary}`,
